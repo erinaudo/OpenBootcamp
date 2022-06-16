0fa0a1feb10b92879e29e67f8846080e1a1641c5
erinaudo <106622909+erinaudo@users.noreply.github.com>
2022-06-15 23:43:23 -0300

    Create Ejercicio05.js

---
+++ JavaScript-Basic/Ejercicio05 - Numbers/Ejercicio05.js
@@ -0,0 +1,16 @@
+let altura_cm = 182;
+let altura_mt = 1.82;
+let peso_kg = 80;
+
+let alt_rup = Math.ceil(altura_mt);
+console.log(alt_rup);
+
+let alt_rdown = Math.floor(altura_mt);
+console.log(alt_rdown);
+
+let max_value = Number.MAX_VALUE + 1;
+if (max_value === Number.MAX_VALUE){
+    console.log("Es igual");
+} else {
+    console.log("Es diferente");
+}
