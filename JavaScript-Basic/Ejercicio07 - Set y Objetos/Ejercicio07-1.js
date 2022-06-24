bd70a995f1754b6cdb25169df7322e823a298a42
erinaudo <106622909+erinaudo@users.noreply.github.com>
2022-06-23 23:37:55 -0300

    Create Ejercicio07-1.js

---
+++ JavaScript-Basic/Ejercicio07 - Set y Objetos/Ejercicio07-1.js
@@ -0,0 +1,13 @@
+const nombresFamilia = ["Emilio", "Solange", "Mateo"]
+
+const setFamily = new Set(nombresFamilia)
+
+console.log(setFamily);
+
+setFamily.add("Emilio")
+
+console.log(setFamily);
+
+setFamily.add("JavaScript")
+
+console.log(setFamily);
